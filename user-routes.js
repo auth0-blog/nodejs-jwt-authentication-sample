a346f3d27ef7ef416c7b26a03c43c5621aac3196
= <ryanchenkie@gmail.com>
2015-07-30 15:04:40 -0600

    Allow for front end to send either username or email as user scheme

--- user-routes.js
+++ user-routes.js
@@ -17,14 +17,28 @@ function createToken(user) {
 }
 
 app.post('/users', function(req, res) {
-  if (!req.body.username || !req.body.password) {
+  var username;
+  var userScheme;
+  
+  // The POST contains a username and not an email
+  if(req.body.username) {
+    username = req.body.username;
+    userScheme = 'username';
+  }
+  // The POST contains an email and not an username
+  else if(req.body.email) {
+    username = req.body.email;
+    userScheme = 'email';
+  }
+
+  if (!req.body.username && !req.body.email || !req.body.password) {
     return res.status(400).send("You must send the username and the password");
   }
-  if (_.find(users, {username: req.body.username})) {
+  if (_.find(users, {username: username}) || _.find(users, {email: username})) {
    return res.status(400).send("A user with that username already exists");
   }
 
-  var profile = _.pick(req.body, 'username', 'password', 'extra');
+  var profile = _.pick(req.body, userScheme, 'password', 'extra');
   profile.id = _.max(users, 'id').id + 1;
 
   users.push(profile);
@@ -35,13 +49,28 @@ app.post('/users', function(req, res) {
 });
 
 app.post('/sessions/create', function(req, res) {
-  if (!req.body.username || !req.body.password) {
+
+  var username;
+  var userScheme;
+  
+  // The POST contains a username and not an email
+  if(req.body.username) {
+    username = req.body.username;
+    userScheme = 'username';
+  }
+  // The POST contains an email and not an username
+  else if(req.body.email) {
+    username = req.body.email;
+    userScheme = 'email';
+  }
+
+  if (!req.body.username && !req.body.email || !req.body.password) {
     return res.status(400).send("You must send the username and the password");
   }
 
-  var user = _.find(users, {username: req.body.username});
+  var user = _.find(users, {username: username}) || _.find(users, {email: username});
   if (!user) {
-    return res.status(401).send("The username or password don't match");
+    return res.status(401).send({message:"The username or password don't match", user: user});
   }
 
   if (user.password !== req.body.password) {
