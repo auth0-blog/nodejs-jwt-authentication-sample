c3e82508d92ae1f326590ec6967deeebb2fc203d
Ryan Chenkie <ryanchenkie@gmail.com>
2015-07-31 17:58:12 -0400

    Put in function to handle determining the user scheme

--- user-routes.js
+++ user-routes.js
@@ -16,29 +16,40 @@ function createToken(user) {
   return jwt.sign(_.omit(user, 'password'), config.secret, { expiresInMinutes: 60*5 });
 }
 
-app.post('/users', function(req, res) {
-  var username;
-  var userScheme;
+function getUserScheme(req) {
   
+  var username;
+  var type;
+
   // The POST contains a username and not an email
   if(req.body.username) {
     username = req.body.username;
-    userScheme = 'username';
+    type = 'username';
   }
   // The POST contains an email and not an username
   else if(req.body.email) {
     username = req.body.email;
-    userScheme = 'email';
+    type = 'email';
   }
+
+  return {
+    username: username,
+    type: type
+  }
+}
+
+app.post('/users', function(req, res) {
+  
+  userScheme = getUserScheme(req);  
 
   if (!req.body.username && !req.body.email || !req.body.password) {
     return res.status(400).send("You must send the username and the password");
   }
-  if (_.find(users, {username: username}) || _.find(users, {email: username})) {
+  if (_.find(users, {username: userScheme.username}) || _.find(users, {email: userScheme.username})) {
    return res.status(400).send("A user with that username already exists");
   }
 
-  var profile = _.pick(req.body, userScheme, 'password', 'extra');
+  var profile = _.pick(req.body, userScheme.type, 'password', 'extra');
   profile.id = _.max(users, 'id').id + 1;
 
   users.push(profile);
@@ -50,25 +61,13 @@ app.post('/users', function(req, res) {
 
 app.post('/sessions/create', function(req, res) {
 
-  var username;
-  var userScheme;
-  
-  // The POST contains a username and not an email
-  if(req.body.username) {
-    username = req.body.username;
-    userScheme = 'username';
-  }
-  // The POST contains an email and not an username
-  else if(req.body.email) {
-    username = req.body.email;
-    userScheme = 'email';
-  }
+  userScheme = getUserScheme(req);
 
   if (!req.body.username && !req.body.email || !req.body.password) {
     return res.status(400).send("You must send the username and the password");
   }
 
-  var user = _.find(users, {username: username}) || _.find(users, {email: username});
+  var user = _.find(users, {username: userScheme.username}) || _.find(users, {email: userScheme.username});
   if (!user) {
     return res.status(401).send({message:"The username or password don't match", user: user});
   }
