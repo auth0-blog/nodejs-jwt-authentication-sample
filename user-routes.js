e90bbe556854f2c75344c9d467d53d4c7ca0c097
Robert Larsson <infinity@your.se>
2015-09-22 20:09:18 +0700

    Update user-routes.js
    You sent req.body.email but referred to req.body.username (which was only in the "db" model), i suggest you use email instead of username on all places.

--- user-routes.js
+++ user-routes.js
@@ -8,7 +8,7 @@ var app = module.exports = express.Router();
 // XXX: This should be a database of users :).
 var users = [{
   id: 1,
-  username: 'gonto',
+  email: 'gonto',
   password: 'gonto'
 }];
 
@@ -17,14 +17,14 @@ function createToken(user) {
 }
 
 app.post('/users', function(req, res) {
-  if (!req.body.username || !req.body.password) {
-    return res.status(400).send("You must send the username and the password");
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send("You must send the email and the password");
   }
-  if (_.find(users, {username: req.body.username})) {
-   return res.status(400).send("A user with that username already exists");
+  if (_.find(users, {username: req.body.email})) {
+   return res.status(400).send("A user with that email already exists");
   }
 
-  var profile = _.pick(req.body, 'username', 'password', 'extra');
+  var profile = _.pick(req.body, 'email', 'password', 'extra');
   profile.id = _.max(users, 'id').id + 1;
 
   users.push(profile);
@@ -35,17 +35,17 @@ app.post('/users', function(req, res) {
 });
 
 app.post('/sessions/create', function(req, res) {
-  if (!req.body.username || !req.body.password) {
-    return res.status(400).send("You must send the username and the password");
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send("You must send the email and the password");
   }
 
-  var user = _.find(users, {username: req.body.username});
+  var user = _.find(users, {email: req.body.email});
   if (!user) {
-    return res.status(401).send("The username or password don't match");
+    return res.status(401).send("The email or password don't match");
   }
 
   if (user.password !== req.body.password) {
-    return res.status(401).send("The username or password don't match");
+    return res.status(401).send("The email or password don't match");
   }
 
   res.status(201).send({
