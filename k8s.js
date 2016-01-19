48cf8ca2b91a3d08766e78bad8cb245860116f4d
Theodore Terdes <tterdes@gmail.com>
2016-01-19 11:33:27 +0800

    read token from file instead

--- k8s.js
+++ k8s.js
@@ -1,6 +1,7 @@
 var express   = require('express'),
     jwt       = require('express-jwt'),
     httpProxy = require('http-proxy'),
+    fs        = require('fs'),
     apiProxy  = httpProxy.createProxyServer(),
     config    = require('./config'),
     quoter    = require('./quoter');
@@ -13,8 +14,15 @@ var jwtCheck = jwt({
 
 var kubernetes = {
   url: process.env.KUBERNETES_API_URL,
-  token: process.env.KUBERNETES_API_TOKEN
+  token: ''
 };
+
+fs.readFile(process.env.KUBERNETES_API_TOKEN_FILE, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  kubernetes.token = data.replace(/\r?\n|\r/g, '');
+});
 
 apiProxy.on('proxyReq', function(proxyReq, req, res, options) {
   proxyReq.setHeader('Authorization', 'Bearer ' + kubernetes.token);
