a9cbde73e2a88bf7f19f9e33758526cf2283a7a8
Kim Maida <kim.maida@auth0.com>
2016-09-16 15:54:24 -0400

    FIX #22 - no longer return empty as a quote.

--- quoter.js
+++ quoter.js
@@ -2,6 +2,6 @@ var quotes = require('./quotes.json');
 
 exports.getRandomOne = function() {
   var totalAmount = quotes.length;
-  var rand = Math.ceil(Math.random() * totalAmount);
+  var rand = Math.ceil(Math.random() * (totalAmount - 1));
   return quotes[rand];
 }
