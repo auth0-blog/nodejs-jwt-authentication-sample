1b960f23b0a9f5102da7b6684cdfafd5774378c8
PROSPER OTEMUYIWA <prosperotemuyiwa@gmail.com>
2017-04-25 12:11:29 +0100

    Add function to create access token and generate JTI

--- user-routes.js
+++ user-routes.js
@@ -16,6 +16,29 @@ function createToken(user) {
   return jwt.sign(_.omit(user, 'password'), config.secret, { expiresIn: 60*60*5 });
 }
 
+function createAccessToken() {
+  return jwt.sign({
+    iss: config.issuer,
+    aud: config.audience,
+    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    scope: 'full_access',
+    sub: "lalaland|gonto",
+    jti: genJti(), // unique identifier for the token
+    alg: 'HS256'
+  }, config.secret);
+}
+
+// Generate Unique Identifier for the access token
+function genJti() {
+  let jti = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 16; i++) {
+      jti += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  
+  return jti;
+}
+
 function getUserScheme(req) {
   
   var username;
@@ -60,7 +83,8 @@ app.post('/users', function(req, res) {
   users.push(profile);
 
   res.status(201).send({
-    id_token: createToken(profile)
+    id_token: createToken(profile),
+    access_token: createAccessToken()
   });
 });
 
@@ -83,6 +107,7 @@ app.post('/sessions/create', function(req, res) {
   }
 
   res.status(201).send({
-    id_token: createToken(user)
+    id_token: createToken(user),
+    access_token: createAccessToken()
   });
 });
